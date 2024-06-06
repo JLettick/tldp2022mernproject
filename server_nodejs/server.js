ca79c88a57eeeca63552a5b8e8f76c0c18e5310a
Justin Grove <26723005+jgrove2@users.noreply.github.com>
2024-06-06 14:40:52 -0400

    Logging for all http traffic (#1)
    * Added Logging
    
    * added files

--- server_nodejs/server.js
+++ server_nodejs/server.js
@@ -4,11 +4,26 @@ import accountRouter from './routes/account-routes.js'
 import customerRouter from './routes/customer-routes.js'
 import eventRouter from './routes/event-routes.js'
 import registrationRouter from './routes/registration-routes.js'
+import { logger } from './utilities/logger.js'
+import morgan from 'morgan';
 
+logger.stream = {
+    write: function(message, encoding) {
+        logger.info(message)
+    }
+}
+
+const stream = {
+    write: (message) => {
+        logger.info(message.trim());
+    }
+}
 init_db()
 
 var app = express()
 app.use(express.json())
+
+app.use(morgan("combined", {stream}))
 
 app.use('/api/accounts', accountRouter)
 app.use('/api/customers', customerRouter)
